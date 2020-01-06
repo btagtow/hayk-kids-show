1136311d94097a96a5011d530e5351023af51cd1
github username <github email address>
2020-01-06 16:10:03 -0700

    completed tasks in README

--- app/controllers/kids_controller.rb
+++ app/controllers/kids_controller.rb
@@ -6,13 +6,19 @@ class KidsController < ApplicationController
   end
 
   def create
-    @kid = Kid.create({
+    @kid = Kid.new({
       username: params[:kid][:username],
       description: params[:kid][:description],
       favorite_color: params[:kid][:favorite_color],
       age: params[:kid][:age],
     })
 
-    render json: { kid: @kid }
+    if @kid.valid?
+      @kid.save
+      render json: { kid: @kid }
+    else 
+      render json: {message: "Username is 7-12 characters and kid must be between 6 and 15."}
+    end 
+
   end
 end
--- app/models/kid.rb
+++ app/models/kid.rb
@@ -1,2 +1,5 @@
 class Kid < ApplicationRecord
+    # validates :age, is: (6..13)
+    validates :username, length: {minimum: 7, maximum: 25}, uniqueness: true
+    validates :age, numericality: { only_integer: true, greater_than: 6, less_than: 15}
 end
